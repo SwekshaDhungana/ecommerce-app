8f1aae6227f6bbe4c51d18e18ca002ccfe8d111a
Sweksha Dhungana <swekshadhungana@gmail.com>
2026-05-13 18:24:53 +0545

    created payment routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 import { connectDB } from "./db/config.js";
 dotenv.config();
 const app = express();
@@ -16,6 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
